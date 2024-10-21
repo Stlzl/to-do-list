8d7ad746153f13d53cffc216aae73de08cf07b9a
Stanislau Zeliankou <stanislau.zeliankou@gmail.com>
2024-10-21 22:09:47 +0200

    - Reworked color palette. - Added date of creation. - Reworked layout. - Reworked delete function.

--- toDoList/src/ItemList.jsx
+++ toDoList/src/ItemList.jsx
@@ -6,16 +6,29 @@ export default function ItemList({items, setItems}) {
     function handleClick(item) {
         console.log(item)
         item.state = true
+        setItems(items.filter((item) => item.state === false))
     }
 
     return (
+        <div className='listContainer'>
         <ul>
-            {items.map( item => 
-                <li key={item.name}>
-                    {!item.state && item.name}{item.state && 'Done !'}
-                    <input type="checkbox" onClick={() => handleClick(item)} />
-                </li>
-            )}
+            <h2>List</h2>
+            <li className='labelItem'>
+                <p className='labelText itemName'>To do</p>
+                <p className='labelText date'>Date</p>
+                <p className='labelText checkbox checkboxLabel'>Mark as done</p>
+            </li>
+            <div className='itemsContainer'>
+                {items.map( item => 
+                    <li key={item.id}>
+                        <p className='itemName'>{item.name}</p>
+                        <p className='date'>{item.date.getDate()} {item.month} {item.date.getFullYear()}</p>
+                        <input type="checkbox" className='checkbox' onClick={() => handleClick(item)} />
+                    </li>
+                    
+                )}
+            </div>
         </ul>
+        </div>
     )
 }
--- toDoList/src/Topbar.jsx
+++ toDoList/src/Topbar.jsx
@@ -5,10 +5,42 @@ export default function Topbar({items, setItems}) {
 
     const [input, setInput] = useState('')
 
+    function getMonthName(date) {
+        switch(date.getMonth()) {
+            case 0:
+                return 'January';
+            case 1:
+                return 'February';
+            case 2:
+                return 'March';
+            case 3:
+                return 'April';
+            case 4:
+                return 'May';
+            case 5:
+                return 'June';
+            case 6:
+                return 'July';
+            case 7:
+                return 'August';
+            case 8:
+                return 'September';
+            case 9:
+                return 'October';
+            case 10:
+                return 'November';
+            case 11:
+                return 'December';
+        }
+    }
+
     function handleClick() {
         setItems(oldItems => [...oldItems, {
             name: input,
-            state: false
+            state: false,
+            date: new Date(),
+            month: getMonthName(new Date()),
+            id: self.crypto.randomUUID()
         }])
     }
 
